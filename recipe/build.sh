46df9a666243037b480c563d3127bbfa3f67ea21
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-11-04 13:38:27 +0100

    uname -o not defined in arm64 builds?

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,7 +18,7 @@ if [[ $(uname -o) != Msys ]]; then
   rm -rf "$SP_DIR/conda/shell/*.exe"
 fi
 "$PYTHON" -I -m conda init --install
-if [[ $(uname -o) == Msys ]]; then
+if [[ $(uname -o || true) == Msys ]]; then
   sed -i "s|CONDA_EXE=.*|CONDA_EXE=\'${PREFIXW//\\/\\\\}\\\\Scripts\\\\conda.exe\'|g" $PREFIX/etc/profile.d/conda.sh
 fi
 
